12b3cb3c79c9ac6a1e3ea39e94d37b2ad2b84229
i@moyeka.ru <KaMoCode@yandex.ru>
2025-09-25 07:08:21 +0900

    Update wsgi.py

--- track/wsgi.py
+++ track/wsgi.py
@@ -9,12 +9,6 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 
 import os
 
-import sys
-
-path = '/home/cdomir/track_new'
-if path not in sys.path:
-    sys.path.append(path)
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'track.settings')
